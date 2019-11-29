add7245c5884c05502550c88659fc7efe25e6624
sakib <ahmadsakibabdullah@gmail.com>
2019-11-29 21:15:53 +0600

    tile-gallery/digital-section/portfolio-area worked.

--- js/main.js
+++ js/main.js
@@ -212,14 +212,18 @@ mainSlider();
   });
 
   
-  // magnific popup
-  $('.popup-img').magnificPopup({
-    type: 'image',
-    gallery: {
-      enabled: true
-    },
-    
-  });
+// Popup gallery
+$('.popup-gallery').magnificPopup({
+  delegate: 'a',
+  type: 'image',
+  tLoading: 'Loading image #%curr%...',
+  mainClass: 'mfp-img-mobile',
+  gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+  }
+});
 
 
 // counterUp
