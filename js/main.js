d31f53b165ce2c947f41cb3102fe43e273332905
sakib <ahmadsakibabdullah@gmail.com>
2019-12-15 11:03:28 +0600

    updated.

--- js/main.js
+++ js/main.js
@@ -101,7 +101,7 @@ mainSlider();
     arrows: false,
     infinite: true,
     speed: 300,
-    slidesToShow: 2,
+    slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [{
         breakpoint: 1024,
@@ -204,6 +204,41 @@ mainSlider();
     });
   });
 
+
+   // team-area-active
+   $('.team-active').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+     
+    ]
+  });
   //for menu active class
   $('.portfolio-menu button').on('click', function (event) {
     $(this).siblings('.active').removeClass('active');
